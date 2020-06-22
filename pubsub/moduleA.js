bd254cc4b4143a3dc6f3df77f2f9bc294ea52b56
drmeloy <drmeloy@gmail.com>
2020-06-22 10:01:29 -0700

    Module A

---
+++ pubsub/moduleA.js
@@ -0,0 +1,10 @@
+const pubSub = require('./pubsub');
+
+module.exports = {
+  publishEvent() {
+    const data = {
+      msg: "TOP SECRET DATA"
+    };
+    pubSub.publish("anEvent", data);
+  }
+};
