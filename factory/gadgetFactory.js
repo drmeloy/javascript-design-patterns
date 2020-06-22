d0bc94f9a937a1fbc9e104a3d874274003af15b8
drmeloy <drmeloy@gmail.com>
2020-06-22 09:51:47 -0700

    factory pattern not working

--- factory/gadgetFactory.js
+++ factory/gadgetFactory.js
@@ -3,9 +3,9 @@ const Tablet = require('./tablet');
 
 const gadget = { Laptop, Tablet };
 
-const createGadget = (type, attributes) => {
-  const GadgetType = gadget[type];
-  return new GadgetType(attributes);
-}
-
-module.exports = createGadget;
+module.exports = {
+  createGadget(type, attributes){
+    const GadgetType = gadget[type];
+    return new GadgetType(attributes);
+  }
+};
