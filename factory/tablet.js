2523c3071e0919ee3788590aef0891b370a26c3c
drmeloy <drmeloy@gmail.com>
2020-06-22 09:34:36 -0700

    Tablet.js

---
+++ factory/tablet.js
@@ -0,0 +1,8 @@
+const Tablet = ({ ram = 0, hdd = 0, name = '', network = 0 }) => {
+  this.ram = ram;
+  this.hdd = hdd;
+  this.name = name;
+  this.network = network;
+}
+
+module.exports = Tablet;
